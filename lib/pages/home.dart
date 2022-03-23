bfcadfe87b8ce9d019fcafee694831a9ea303ab9
MhRivaldo <aldorival279@gmail.com>
2022-03-24 00:09:15 +0700

    daftar belanja

---
+++ lib/pages/home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:jobsheet5/models/item.dart';
+import 'package:jobsheet5/pages/item_page.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belanja App'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final Item = items[index];
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, ItemPage.routeName,
+                    arguments: Item);
+              },
+              child: Card(
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(Item.name),
+                      ),
+                      Expanded(
+                        child: Text(
+                          Item.price.toString(),
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
